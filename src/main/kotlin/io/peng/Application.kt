b9730260d4b305e0daa2e2ec16b8bf8a9fbb07c5
alfred <alfredsklotau@yahoo,com>
2022-01-12 22:01:15 +0000

    Initial commit, empty project to start

---
+++ src/main/kotlin/io/peng/Application.kt
@@ -0,0 +1,13 @@
+package io.peng
+
+import io.ktor.application.*
+import io.peng.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+    configureSerialization()
+}
